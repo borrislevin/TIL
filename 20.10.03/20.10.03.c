9b5d933e6f9537ed406f5e54263c52ab44adc446
borrislevin <accsccc@gmail.com>
2020-10-04 16:17:52 +0900

    20.10.04

--- 20.10.03/20.10.03.c
+++ 20.10.03/20.10.03.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	printf("Be happy!\n");
+	int a,i,temp;
+	i = 1;
+	srand((unsigned)time(NULL));
+	a = rand() % 100;
+
+	printf(">>>Up & Down 맞추기<<<\n\n");
+
+	while (i <= 10) {
+		printf("%d번째 기회입니다. 숫자를 맞춰주세요~ : ", i);
+		scanf_s("%d", &temp);
+		i++;
+		if (temp == a) {
+			printf("정답입니다~!\n");
+			return 0;
+		}
+		else if (temp < a)
+			printf("Up Up 더 높은 숫자를 입력하세요.\n");
+		else
+			printf("Down Down 더 낮은 숫자를 입력하세요.\n");
+	}
+	printf("실패하였습니다!!\n");
+
+
 }
---
+++ 20.10.04/20.10.04/변수 크기.c
@@ -0,0 +1,14 @@
+/*
+자료형				크기		입력 변환 문자
+(unsigned)short		2		%hd
+(unsigned)int		4		%d
+(unsigned)long		4		%ld
+(unsigned)long long	8		%lld
+
+float				4		%f
+double				8		%lf
+long double			8,10,12,16	%Lf
+
+char				1		%c
+char 배열			가변적	%s
+*/
